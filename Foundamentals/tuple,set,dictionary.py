c02a293974f5a601dae3667b61aea44a7afc4db2
Shamil Aliyew <shamil.aliyev005@gmail.com>
2025-03-18 21:18:57 +0400

    Task daha optimal yazildi

--- Foundamentals/tuple,set,dictionary.py
+++ Foundamentals/tuple,set,dictionary.py
@@ -1,19 +1,41 @@
+# l=[]
+# firsList=['*']
+# s=1
+# say=97
+# s1=int(input())
+# s2=int(input())
+# for i in range(s1-1):
+#     firsList.append(str(s))
+#     s+=1
+# for x in range (s2):
+#     b = []
+#     b.append(chr(say))
+#     say+=1
+#     for i  in range(s1-1):
+#         b.append("*")
+#     l.append(b)
+# print(firsList)
+# for x in range (len(l)):
+#     print(l[x])
+
+
+# Daha optimal
 l=[]
-firsList=['*']
-s=1
-say=97
-s1=int(input())
-s2=int(input())
-for i in range(s1-1):
-    firsList.append(str(s))
-    s+=1
-for x in range (s2):
-    b = []
-    b.append(chr(say))
-    say+=1
-    for i  in range(s1-1):
-        b.append("*")
-    l.append(b)
-print(firsList)
-for x in range (len(l)):
-    print(l[x])
+start = ord('a')-1
+for i in range(4):
+    a=[]
+    for j in range(4):
+        if i==0:
+            if j==0:
+                j='*'
+        else:
+            if j==0:
+                j=chr(start)
+            else:
+                j='x'
+        a.append(format(str(j),' >2'))
+    start +=1
+    l.append(a)
+
+for i in l:
+    print("".join(i))
